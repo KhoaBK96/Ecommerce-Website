c4198d433c569208f476dc1f573334c88cf90619
khoabk28996@gmail.com <khoa.tx28@gmail.com>
2022-03-14 11:53:22 +0700

    update

--- js/product.js
+++ js/product.js
@@ -1,10 +1,52 @@
-export const product = [
-  { title: black - shirt, img: "./img/product1.jpg", price: 12.99 },
-  { title: airpod, img: "./img/product2.jpg", price: 30.99 },
-  { title: black - jacket, img: "./img/product3.jpg", price: 20.99 },
-  { title: glasses, img: "./img/product4.jpg", price: 5.99 },
-  { title: waterbottle, img: "./img/product5.jpg", price: 8.99 },
-  { title: black - hat, img: "./img/product6.jpg", price: 20.99 },
-  { title: bag, img: "./img/product7.jpg", price: 18.99 },
-  { title: shoes, img: "./img/product8jpg", price: 21.99 },
+const product = [
+  {
+    title: "black-shirt",
+    img: "../img/product1.jpg",
+    price: 12.99,
+    category: "clothes",
+  },
+  {
+    title: "airpod",
+    img: "../img/product2.jpg",
+    price: 30.99,
+    category: "accessories",
+  },
+  {
+    title: "black-jacket",
+    img: "../img/product3.jpg",
+    price: 20.99,
+    category: "clothes",
+  },
+  {
+    title: "glasses",
+    img: "../img/product4.jpg",
+    price: 5.99,
+    category: "accessories",
+  },
+  {
+    title: "waterbottle",
+    img: "../img/product5.jpg",
+    price: 8.99,
+    category: "accessories",
+  },
+  {
+    title: "black-hat",
+    img: "../img/product6.jpg",
+    price: 20.99,
+    category: "clothes",
+  },
+  {
+    title: "bag",
+    img: "../img/product7.jpg",
+    price: 18.99,
+    category: "accessories",
+  },
+  {
+    title: "shoes",
+    img: "../img/product8.jpg",
+    price: 21.99,
+    category: "shoes",
+  },
 ];
+
+export default product;
